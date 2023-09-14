69ed4e9185ff6c69e1dfe6b894d1ef227395cd59
Yazmin Perez <zoeperez333@gmail.com>
2023-09-13 17:36:59 -0700

    add dotenv, introduce API_URL environment variable, and display comics

--- comic-dex/src/App.js
+++ comic-dex/src/App.js
@@ -1,10 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';import './App.css';
+import axios from 'axios';
+import { API_URL } from './constants';
 
 function App() {
+
+  const [comics, setComics] = useState([]);
+
+  useEffect(() => {
+    axios.get(API_URL).then(response => {
+      setComics(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching comics: ', error);
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1>ComicDex</h1>
+      <h2>Comics List</h2>
+        {comics.map(comic => (
+          <div key={comic.id} className="post-container">
+            <h3>{comic.title}</h3>
+            <p>{comic.description}</p>
+          </div>
+        ))}
     </div>
   );
 }
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # where your request comes from (react app's IP address)
-    origins "http://localhost:3000/"
+    origins 'http://localhost:3001'
 
-    resource "",
+    resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      # indicates that a request can be made using cookies
+      credentials: true
   end
 end
